962f94e5f8ef12012d1d488e2cd8df09961d0c21
tomfelix <>
2020-05-04 15:25:34 +0200

    added fetch movie detail logic

--- screens/MovieDetailScreen.js
+++ screens/MovieDetailScreen.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { Text, Image } from 'react-native-elements';
+import Axios from 'axios';
+
+const API_KEY = '22d3bf9dd261998ae9158dcedfb8b4b9';
 
 const MovieDetailScreen = (props) => {
+  const [data, setData] = useState([]);
+
+  const movieId = props.navigation.getParam('movieId');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await Axios(
+        `https://api.themoviedb.org/3/movie/${movieId}?
+        &api_key=${API_KEY}
+        `
+      );
+
+      setData(result);
+    };
+
+    fetchData();
+  }, [movieId]);
+
+  console.log('movieId', movieId, data?.data?.title);
+  if (data.length == 0) {
+    return (
+      <View>
+        <Text>loading</Text>
+      </View>
+    );
+  }
   return (
     <View>
-      <Text>title</Text>
+      <Text>{data.data.title}</Text>
       <Image source={{ uri: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg' }} />
-      <Text>popularity</Text>
-      <Text>rating</Text>
       <Text>gatunek</Text>
-      <Text>desc</Text>
+      {data.data.genres.map((genre) => (
+        <Text key={genre.id}>{genre.name}</Text>
+      ))}
+      <Text>{data.data.overview}</Text>
       <Text>country of production</Text>
+      {data.data.production_countries.map((country) => (
+        <Text key={country.id}>{country.name}</Text>
+      ))}
     </View>
   );
 };
--- screens/MoviesScreen.js
+++ screens/MoviesScreen.js
@@ -7,24 +7,6 @@ import MovieCard from '../components/MovieCard';
 
 const API_KEY = '22d3bf9dd261998ae9158dcedfb8b4b9';
 
-const list = [
-  {
-    name: 'Amy Farha',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-    subtitle: 'Vice President'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  },
-  {
-    name: 'Chris Jackson',
-    avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-    subtitle: 'Vice Chairman'
-  }
-];
-
 const MoviesScreen = (props) => {
   const keyExtractor = (item, index) => index.toString();
   const [query, setQuery] = useState('batman');
